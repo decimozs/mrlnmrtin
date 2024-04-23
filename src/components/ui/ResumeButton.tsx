25b1788ea32d497647f4129b1d5f4bf37a376d2f
decimozs <marlonadiguemartin548@gmail.com>
2024-04-23 17:32:28 +0800

    new features added

--- src/components/ui/ResumeButton.tsx
+++ src/components/ui/ResumeButton.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ResumeButton() {
   return (
-    <Link href="/" className="text-slate mt-[-0.2rem] resume tooltip">
+    <Link href="/" className=" text-slate mt-[-0.2rem] resume tooltip">
       Resume
     </Link>
   );
