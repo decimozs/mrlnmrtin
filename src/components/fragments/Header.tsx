41c6605827dfc56c2ad6f5975c5b25ae78403a83
decimozs <marlonadiguemartin548@gmail.com>
2024-04-23 17:42:30 +0800

    new features added

--- src/components/fragments/Header.tsx
+++ src/components/fragments/Header.tsx
@@ -3,7 +3,7 @@ import { Time } from "../ui";
 export default function Header() {
   return (
     <header className="flex flex-col gap-4">
-      <h1 className="text-[1.3rem]">Hello, I&apos;m Martin</h1>
+      <h1 className="text-[1.3rem]">Hello, I&apos;m Martin 👋</h1>
       <p className="text-slate max-w-[401px] mb-4">
         I&apos;m a creative developer and a student, building innovative
         products within the digital space.
