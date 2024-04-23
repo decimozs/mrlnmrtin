a419347b70dadf06727e4f9adf1cbf3fe3409db0
decimozs <marlonadiguemartin548@gmail.com>
2024-04-23 20:00:32 +0800

    new features added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
     <main className="w-full h-screen flex items-center justify-center flex-col text-center gap-3">
       <h1>Whoops! Looks like this portfolio piece is taking a coffee break.</h1>
       <p className="w-[350px]">
-        No worries, though! While it's out enjoying some downtime, why not take
-        a moment to check out some of my other fantastic projects? They're all
-        ready and waiting for you!"
+        No worries, though! While it&apos;s out enjoying some downtime, why not
+        take a moment to check out some of my other fantastic projects?
+        They&apos;re all ready and waiting for you!
       </p>
       <Link href="/" className="text-slate">
         Index
