271677d7bd66d7e5600c897aa89c8c846b34a537
decimozs <marlonadiguemartin548@gmail.com>
2024-04-23 11:27:16 +0800

    new features added

--- src/components/fragments/Header.tsx
+++ src/components/fragments/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <header className="flex flex-col gap-4">
-      <h1 className="text-[1.3rem]">Hello, I'm Martin</h1>
+      <h1 className="text-[1.3rem]">Hello, I&apos;m Martin</h1>
       <p className="text-slate max-w-[401px] mb-4">
-        I’m a creative developer and a student, building innovative products
-        within the digital space.
+        I&apos;m a creative developer and a student, building innovative
+        products within the digital space.
       </p>
     </header>
   );
