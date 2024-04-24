76055c4c105d413746cf7e51e0b8df8d49f3edd6
decimozs <marlonadiguemartin548@gmail.com>
2024-04-24 18:00:55 +0800

    added version navigation

--- src/components/ui/Version.tsx
+++ src/components/ui/Version.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Version() {
   return (
     <div className="hidden w-full lg:fixed lg:flex items-center justify-center left-0 bottom-4">
-      <h1>+ Version 3</h1>
+      <Link href="https://github.com/decimozs/mrlnmrtin">+ Version 3</Link>
     </div>
   );
 }
