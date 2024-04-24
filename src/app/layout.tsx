1df62ec14f54edc4015541556bbe6a9b13c9f4b5
decimozs <marlonadiguemartin548@gmail.com>
2024-04-24 21:54:19 +0800

    fix style path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
 import { NeueMontreal } from "../../public/font";
 import { Nav } from "@/components/fragments";
 import { seo } from "@/config";
