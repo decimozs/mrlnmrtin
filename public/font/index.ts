31ebff41f2609bfe33fdefd51eb43a473d6b428a
decimozs <marlonadiguemartin548@gmail.com>
2024-04-29 21:33:09 +0800

    chore: delete unused fonts

--- public/font/index.ts
+++ public/font/index.ts
@@ -3,16 +3,6 @@ import localFont from "next/font/local";
 export const NeueMontreal = localFont({
   src: [
     {
-      path: "./NeueMontreal-Light.otf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./NeueMontreal-LightItalic.otf",
-      weight: "300",
-      style: "italic",
-    },
-    {
       path: "./NeueMontreal-Regular.otf",
       weight: "400",
       style: "normal",
@@ -26,11 +16,6 @@ export const NeueMontreal = localFont({
       path: "./NeueMontreal-MediumItalic.otf",
       weight: "500",
       style: "italic",
-    },
-    {
-      path: "./NeueMontreal-Bold.otf",
-      weight: "700",
-      style: "normal",
     },
   ],
 });
