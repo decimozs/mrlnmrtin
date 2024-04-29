c26eb47843340f85d72b007abf6eb64c5eca7b8f
decimozs <marlonadiguemartin548@gmail.com>
2024-04-29 20:27:21 +0800

    chore: fix time meridiem

--- src/helpers/getCurrentTime.ts
+++ src/helpers/getCurrentTime.ts
@@ -2,6 +2,6 @@ export default function getCurrentTime(): string {
   const now: Date = new Date();
   const hours: number = now.getHours() % 12 || 12;
   const minutes: string = now.getMinutes().toString().padStart(2, "0");
-  const meridiem: string = hours >= 12 ? "PM" : "AM";
+  const meridiem: string = hours >= 12 ? "AM" : "PM";
   return `${hours}:${minutes} ${meridiem}`;
 }
